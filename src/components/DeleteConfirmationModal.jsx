73242c213129d76a1fb487a0fa35c95ce43bb7ee
ArturChernets <arturchernec2006@gmail.com>
2025-03-14 12:55:17 +0200

    Added entire project

---
+++ src/components/DeleteConfirmationModal.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "../styles/DeleteConfirmationModal.module.css"; // Підключаємо CSS файл
+
+function DeleteConfirmationModal({ student, closeModal, handleDelete }) {
+    return (
+        <div className={styles.modal}>
+            <div className={styles.modalContent}>
+                <div className={styles.modalHeader}>
+                    <h3>Are you sure you want to delete user {student.firstName} {student.lastName}?</h3>
+                </div>
+                <div className={styles.modalFooter}>
+                    <button className={styles.cancelButton} onClick={closeModal}>
+                        Cancel
+                    </button>
+                    <button
+                        className={styles.createButton}
+                        onClick={() => { handleDelete(student.id); closeModal(); }}
+                    >
+                        OK
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DeleteConfirmationModal;
