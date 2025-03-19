5b13decde90206d788cd32cd3cc3b1694f4dc551
ArturChernets <arturchernec2006@gmail.com>
2025-03-19 16:23:59 +0200

    Add project for deploying

---
+++ src/components/Menu.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../styles/Menu.module.css";
+
+function Menu() {
+    return (
+        <div className={styles.menu}>
+            <NavLink
+                to="/dashboard"
+                className={({ isActive }) => `${styles.navItem} ${isActive ? styles.active : ''}`}
+            >
+                Dashboard
+            </NavLink>
+            <NavLink
+                to="/students"
+                className={({ isActive }) => `${styles.navItem} ${isActive ? styles.active : ''}`}
+            >
+                Students
+            </NavLink>
+            <NavLink
+                to="/tasks"
+                className={({ isActive }) => `${styles.navItem} ${isActive ? styles.active : ''}`}
+            >
+                Tasks
+            </NavLink>
+        </div>
+    );
+}
+
+export default Menu;
